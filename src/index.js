3a76577ca27b8508c24dbdaf73f0cf439fed67c5
barbwirezeuz <soceeenin@yahoo.com>
2023-12-06 12:18:47 +0100

    bf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Todo from "./components/todo";
+import Todo from "./todo";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
